ce1b09ef4f179230def63b743065c8e81e6abd84
Chrsty02 <chrsty02@outlook.com>
2025-04-08 17:29:03 -0400

    update

---
+++ src/assignment1/Sum.java
@@ -0,0 +1,13 @@
+package assignment1;
+
+public class Sum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Sum");
+		
+		int = 4; //4 bytes
+		int = 36; //2 bytes
+	}
+
+}
